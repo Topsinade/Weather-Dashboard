83322598f37d9cafcad082a5c172a6d4c15d78f5
Temitope <topsinade@gmail.com>
2024-01-28 19:29:03 +0000

    API for 6 days added

--- starter/script.js
+++ starter/script.js
@@ -1,43 +1,27 @@
-$(document).ready(function() {
-
-var searchString = "";
-
-var limit = 5;
-var apiKey= "6168024906cbfc2eddabc6965434b217";
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="+searchString+"&limit=" + limit + "&appid=" + apiKey;
-
-
-
-$("#search-button").on("click", function () {
-    searchString = $("#search-string").val();
-    // console.log('query: ', queryURL)
-
-
-    fetch(queryURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-    console.log(data);
-    console.log(data)
-    
+$(document).ready(function () {
+    $("#search-button").on("click", function (event) {
+    event.preventDefault();
+      var searchString = $("#search-input").val();
+      var cnt = 6;
+      var apiKey = "6168024906cbfc2eddabc6965434b217";
+      var queryURL =
+        "http://api.openweathermap.org/data/2.5/forecast/daily?q=" +
+        searchString +
+        "&cnt=" +
+        cnt +
+        "&appid=" +
+        apiKey;
+  
+      console.log(searchString);
+  
+      fetch(queryURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
     });
-});
+  });
 
-});
-
-// $(document).ready(function() {
-//     // Attach a click event handler to the button
-//     $('#searchButton').click(function() {
-//       // Get the value of the search input using jQuery
-//       var searchValue = $('#searchInput').val();
-
-//       // Use the searchValue variable as needed (e.g., log it to the console)
-//       console.log("Search Value:", searchValue);
-
-//       // You can perform further actions with the searchValue variable
-//       // For example, initiate an AJAX request, update the DOM, etc.
-//     });
-//   });
-
-
+//   api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
