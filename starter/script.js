2a1ecb9fc8c34c275c2b2baff66e033847c6b467
Temitope <topsinade@gmail.com>
2024-01-29 21:53:16 +0000

    Api resolved and working perfectly, view result on console

--- starter/script.js
+++ starter/script.js
@@ -5,24 +5,33 @@ $(document).ready(function () {
       var cnt = 6;
       var apiKey = "6168024906cbfc2eddabc6965434b217";
       var queryURL =
-        "http://api.openweathermap.org/geo/1.0/direct?q=" +
-        searchString +
-        "&cnt=" +
-        cnt +
-        "&appid=" +
-        apiKey;
-  
+        "http://api.openweathermap.org/data/2.5/weather?q=" + searchString + "&appid=" + apiKey
+      
       console.log(searchString);
-  
+//   Fetch to get the wather data
       fetch(queryURL)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
           console.log(data);
+          localStorage.setItem("searchString", JSON.stringify(searchString));
+
         });
+
 
     });
   });
-
-//   api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={API key}
+  var searchString = $("#search-input").val();
+  renderSearch();
+  function renderSearch() {
+    history.innerHTML = "";
+    for (var i = 0; i < searchString.length; i++) {
+        var list = searchString[i];
+    
+        var li = document.createElement("li");
+        li.textContent = list;
+        history.appendChild(li);
+    }
+  }
+  
